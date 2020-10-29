66ef1c8f59efcda608dd8791f6becebb0c85cc79
wang-jie-2020 <421004034@qq.com>
2020-10-29 10:41:26 +0800

    basic test

--- Sample.ConsoleApp/Program.cs
+++ Sample.ConsoleApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Sms;
+using Sms.ChinaMobile;
 
 namespace Sample.ConsoleApp
 {
@@ -13,13 +16,17 @@ namespace Sample.ConsoleApp
             services.AddLogging(x => x.AddConsole());
             services.AddSms(x =>
             {
+                x.Version = "1.00";
                 x.UseChinaMobile(z =>
                 {
-
+                    z.EcName = "test";
                 });
             });
 
             var sp = services.BuildServiceProvider();
+
+            //var t1 = sp.GetRequiredService<IOptions<SmsOptions>>();
+            //var t2 = sp.GetRequiredService<IOptions<ChinaMobileOptions>>();
 
             //sp.GetService<ISmsService>().Send();
 
--- Sms.ChinaMobile/Sms.ChinaMobileOptions.cs
+++ Sms.ChinaMobile/Sms.ChinaMobileOptions.cs
@@ -35,13 +35,19 @@ namespace Sms.ChinaMobile
 
         public void Configure(ChinaMobileOptions options)
         {
-            //if (options.DbContextType != null)
-            //{
-            //    using var scope = _serviceScopeFactory.CreateScope();
-            //    var provider = scope.ServiceProvider;
-            //    using var dbContext = (DbContext)provider.GetRequiredService(options.DbContextType);
-            //    options.ConnectionString = dbContext.Database.GetDbConnection().ConnectionString;
-            //}
+            /*
+             *  Do Extra Configure
+             *  example:
+             *
+                    if (options.DbContextType != null)
+                    {
+                        using var scope = _serviceScopeFactory.CreateScope();
+                        var provider = scope.ServiceProvider;
+                        using var dbContext = (DbContext)provider.GetRequiredService(options.DbContextType);
+                        options.ConnectionString = dbContext.Database.GetDbConnection().ConnectionString;
+                    }
+             *
+             */
         }
     }
 }
